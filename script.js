7a0853fe4039f190b092445540f2a062f6b16684
André Vinícius Leicht <107876986+Andre-Leicht@users.noreply.github.com>
2022-06-23 16:48:30 -0300

    Botão de apagar tudo

---
+++ script.js
@@ -0,0 +1,23 @@
+const andre = document.querySelector('input')
+const joao = document.querySelector('ol')
+let oseias = []
+
+function Adicionar() {
+    oseias.push(andre.value)
+    console.log(oseias)
+    render()
+    andre.value = ''
+}
+
+function render(){
+    joao.innerHTML = null
+    oseias.forEach(function(tarefa){
+    const li = document.createElement('li')
+    li.innerText = tarefa
+    joao.appendChild(li)
+    })
+}
+function Apagar(){
+    oseias = []
+    joao.innerHTML = null
+}
